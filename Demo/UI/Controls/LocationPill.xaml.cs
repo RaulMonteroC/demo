da35ecb6a768e97d6e8733cee125883f589b3bee
Raul Montero <raul@monteroc.com>
2020-05-21 14:16:26 -0400

    Adjust UI for small screens

--- Demo/UI/Controls/LocationPill.xaml.cs
+++ Demo/UI/Controls/LocationPill.xaml.cs
@@ -7,6 +7,30 @@ namespace Demo.UI.Controls
 {
     public partial class LocationPill : ContentView
     {
+
+        public string Distance
+        {
+            get => (string)GetValue(DistanceProperty);
+            set => SetValue(DistanceProperty, value);
+        }
+
+        public static readonly BindableProperty DistanceProperty =
+            BindableProperty.Create(
+                propertyName: "Distance",
+                returnType: typeof(string),
+                declaringType: typeof(LocationPill),
+                defaultValue: default(string),
+                defaultBindingMode: BindingMode.OneTime,
+                propertyChanged: OnPropertyChanged);
+
+        private static void OnPropertyChanged(BindableObject bindable, object oldValue, object newValue)
+        {
+            if (bindable is LocationPill control)
+            {
+                control.DistanceLabel.Text = newValue.ToString();
+            }
+        }
+
         public LocationPill()
         {
             InitializeComponent();
