c1d559a14378f417a3b4452ba83407262f9822b0
Raul Montero <raul@monteroc.com>
2020-05-21 13:07:57 -0400

    Simplify XAML with custom controls

---
+++ Demo/UI/Controls/HeaderImageSlider.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace Demo.UI.Controls
+{
+    public partial class HeaderImageSlider : ContentView
+    {
+        public HeaderImageSlider()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ Demo/UI/Controls/LocationPill.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace Demo.UI.Controls
+{
+    public partial class LocationPill : ContentView
+    {
+        public LocationPill()
+        {
+            InitializeComponent();
+        }
+    }
+}
